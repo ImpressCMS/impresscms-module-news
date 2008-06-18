6cffc8198321b858fd01ad6728f12775ed6b2564
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-18 09:15:24 +0000

    Modules - [news] - 1.54 - german - job by Sato-San for unification with files english 1.56, start...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/news/trunk@2987 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ news/language/german/newsletter.php
@@ -0,0 +1,17 @@
+<?php
+$newslettertemplate=<<<contentend
+Titel: %title%
+Thema: %topic_title%
+Geschrieben von: %publisher%
+Veröffentlicht am: %published%
+Bewertung: %rating%
+Gelesen: %reads%
+
+Inhalt:
+%hometext%
+
+Mehr lesen: %link%
+ ----------------------------------------------------------------------------
+
+contentend;
+?>
